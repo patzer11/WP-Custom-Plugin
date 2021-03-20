4637b3a59e2fb4afab32942996e2f31bc61950c7
patzer11 <lukaspatzer@hotmail.com>
2021-03-20 23:31:17 +0100

    Adding code (Creates a new wp admin dashboard menu page and subpage)

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Custom Plugin
+Plugin URI:
+Description: Plugin creating a custom page in your wordpress dashboard
+Author: Lukas Patzer
+Author URI:
+Version: 0.1
+*/
+
+add_action("admin_menu", "addMenu");
+function addMenu()
+{
+  add_menu_page("Example Options", "Example Options", 4, "example-options", "exampleMenu" );
+  add_submenu_page("example-options", "Option 1", "Option 1", 4, "example-option-1", "option1");
+}
+
+function exampleMenu()
+{
+echo <<< 'EOD'
+  <h2> Coming Soon</h2>
+EOD;
+}
+
+function option1()
+{
+  echo "Sida 2";
+}
