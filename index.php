c0826b395fa9ae4e036dc39c9f1d80b697f71250
patzer11 <lukaspatzer@hotmail.com>
2021-03-21 14:45:36 +0100

    Removing sub-page, changing page name to "To do list"

--- index.php
+++ index.php
@@ -11,27 +11,13 @@ Version: 0.1
 add_action("admin_menu", "addMenu");
 function addMenu()
 {
-  add_menu_page("Example Options", "Example Options", 4, "example-options", "exampleMenu" );
-  add_submenu_page("example-options", "Option 1", "Option 1", 4, "example-option-1", "option1");
+  add_menu_page("To do list", "To do list", 4, "to-do-list", "todoMenu" );
+
 }
 
-function exampleMenu()
+function todoMenu()
 {
 echo <<< 'EOD'
   <h2> Coming Soon</h2>
 EOD;
-}
-
-function option1()
-{
-  echo <<< 'EOD'
-  <form action="/html/tags/html_form_tag_action.cfm" method="post">
-  <div>
-  <textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;">
-  To do list!
-  </textarea>
-  </div>
-  <input type="submit" value="Submit">
-  </form>
-  EOD;
 }
